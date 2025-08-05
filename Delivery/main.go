a67d84d4af81cb745be546a10f6c8982f233a1b3
muhammed-shamsadin <muhammedsamsoon@gmail.com>
2025-08-05 23:58:00 +0300

    feat: create, get, delete, update, and get specific blog are implemented

--- Delivery/main.go
+++ Delivery/main.go
@@ -1,1 +1,14 @@
 package main
+
+import (
+	"a2sv-project/Delivery/routers"
+	"a2sv-project/Infrastructure/database"
+)
+
+func main() {
+	// Connect to the database
+	database.ConnectDB()
+	
+	r := routers.SetupRoutes()
+	r.Run(":8080")
+}
